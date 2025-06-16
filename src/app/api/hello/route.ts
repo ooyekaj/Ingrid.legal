287a6213670a665f888675ce0591ddc1f2665135
Kyle Tu <kyle.tu@scale.com>
2025-06-15 18:01:09 -0700

    update

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
 	try {
-		const response = await fetch("http://ingrid.legal/api/hello");
+		const response = await fetch("https://ingrid.legal/api/hello");
 		const data = await response.json();
 		return NextResponse.json(data);
 	} catch (error) {
--- src/app/api/judges/route.ts
+++ src/app/api/judges/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
 	try {
-		const response = await fetch("http://ingrid.legal/api/judges");
+		const response = await fetch("https://ingrid.legal/api/judges");
 		const data = await response.json();
 		return NextResponse.json(data);
 	} catch (error) {
